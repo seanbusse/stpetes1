7a98f9ac2b715dd925a58c2555a40a5098ba44ce
Sean Busse <sean@4uweb.design>
2020-11-20 11:17:41 -0700

    Added privacy and sermons pages

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
-// Test that index.js is properly loaded
-$(document).ready(function() {
-  console.log('Your viewport width is ' + $(window).width() + 'px');
-});
+setMenuBehavior();
 
 // Hide the plain-text email addresses
 var string1 = "info";
@@ -12,3 +9,37 @@ var string4 = string1 + string2 + string3;
 $(document).ready(function() {
   $('.emailAddress').html("<a href=mailto:" + string1 + string2 + string3 + ">" + " " + string4 + "</a>");
 });
+
+// Show window size on resize
+$(window).resize(function() {
+  var windowSize = $(window).width();
+  $('.windowWidthChange').html('<code style="padding: 3px 10px; color: #333333;">' + windowSize + 'px' + '</code>');
+  setTimeout(function() {
+    $('.windowWidthChange').css('display','none');
+  }, 1000);
+  setMenuBehavior();
+});
+
+// Change default menu behavior on load
+function setMenuBehavior() {
+  // Get the window windowSize
+  var windowSize = $(window).width();
+  // Set Behavior
+  if(windowSize >= 992) {
+    $('.dropdown').mouseover(function() {
+      $(this).children('.dropdown-menu').show();
+    });
+    $('.dropdown').mouseout(function() {
+      $(this).children('.dropdown-menu').hide();
+    });
+    $('.dropdown-menu').mouseover(function() {
+      $('.dropdown-manu').show();
+    });
+    $('.dropdown-menu').mouseout(function() {
+      $('.dropdown-manu').hide();
+    });
+  } else if(windowSize <= 991) {
+    $('.dropdown').off('mouseover');
+    $('.dropdown').off('mouseout');
+  }
+}
