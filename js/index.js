8d34cbfc6ae0c37f51bf5ca92502959c7e990cb4
Sean Busse <sean@4uweb.design>
2020-11-25 12:37:41 -0700

    Finally solved the menu customization

--- js/index.js
+++ js/index.js
@@ -6,15 +6,51 @@ var string4 = string1 + string2 + string3;
 $('.emailAddress').html("<a href=mailto:" + string1 + string2 + string3 + ">" + " " + string4 + "</a>");
 
 // Set mobile Home behavior
-$('.homeLink').click(function() {
+$('.hasLink').click(function() {
   var menuSize = $(window).width();
-  var dropdownDisplay = $('.homeDropdown').css('display');
+  var dropdownDisplay = $(this.parentNode).children('.dropdown-menu').css('display');
+  var link = $(this).attr('value');
   if (dropdownDisplay == 'none' && menuSize < 992) {
-    $('.homeLink').attr('href', '#');
+    $(this.parentNode).children('.dropdown-menu').slideToggle();
+    $(this.parentNode).children('.mobileView').children('.mobileClose').show();
+    $(this.parentNode).children('.mobileView').children('.mobileOpen').hide();
+    $('.hasLink').attr('href', '#');
   } else {
-    $('.homeLink').attr('href', 'index');
+    $('.hasLink').attr('href', link);
   }
 });
+
+// Set new dropdown-toggle behavior
+$('.noLink').click(function() {
+  var menuSize = $(window).width();
+  if (menuSize < 992) {
+    $(this.parentNode).children('.dropdown-menu').slideToggle();
+    $(this.parentNode).children('.mobileView').children('.mobileClose').toggle();
+    $(this.parentNode).children('.mobileView').children('.mobileOpen').toggle();
+  }
+});
+
+// Add a open/close button/indicator for dropdown menu
+var screenSize = $(window).width();
+if (screenSize < 992) {
+  $('.mobileView').click(function() {
+    $(this).children('.mobileOpen').toggle();
+    $(this).children('.mobileClose').toggle();
+    $(this.parentNode).children('.dropdown-menu').slideToggle();
+  });
+
+  // $('.mobileClose').click(function() {
+  //   $(this.parentNode).children('.dropdown-menu').slideUp();
+  //   $(this).toggle();
+  //   $(this.parentNode).children('.mobileOpen').show();
+  //   $('.mobileClose').mouseout(function() {
+  //     $('.dropdown').click(function() {
+  //       $(this).children('.dropdown-menu').slideDown();
+  //     });
+  //   });
+  // });
+}
+
 // Change default menu behavior on load or resize
 function setMenuBehavior() {
   var size = $(window).width();
@@ -28,17 +64,14 @@ function setMenuBehavior() {
     $('.dropdown').mouseout(function() {
       $(this).children('.dropdown-menu').hide();
     });
-    $('.homeLink').attr('href', 'index');
+    $('.homeLink').attr('href', '/');
+    $('.mobileView').hide();
   } else {
     $('.dropdown').off('mouseover');
     $('.dropdown').off('mouseout');
-    $('.dropdown').click(function() {
-      $(this).children('.dropdown-menu').show();
-    });
   }
-  console.log(menuDisplay  + ' ' + size + 'px');
+  console.log(menuDisplay + ' ' + size + 'px');
 }
-
 
 $(document).ready(function() {
   var startingSize = $(window).width();
