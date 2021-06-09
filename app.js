68a69dd8eed45c3d522c0b694cd64f76c73b311b
Sean Busse <seanbusse@Seans-Mac-mini.local>
2021-06-08 23:12:27 -0600

    Made some changes to the way the copyright year is inserted and made some changes to the local-churches and links on home page

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const ejs = require('ejs');
 const date = require(__dirname + '/date.js');
-const year = date.getYear();
 
 const app = express();
 
@@ -12,64 +11,57 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
 app.get('/', function(req, res) {
-  res.render('home', {copyrightYear: year})
+  res.render('home')
 });
 
 app.get('/about', function(req, res) {
-  res.render('about', {copyrightYear: year})
+  res.render('about')
 });
 
 app.get('/contact', function(req, res) {
-  res.render('contact', {copyrightYear: year})
+  res.render('contact')
 });
 
 app.get('/desktop-calendar', function(req, res) {
-  res.render('desktop-calendar', {copyrightYear: year})
+  res.render('desktop-calendar')
 });
 
 app.get('/give', function(req, res) {
-  res.render('give', {copyrightYear: year})
+  res.render('give')
 });
 
 app.get('/lectionary', function(req, res) {
-  res.render('lectionary', {copyrightYear: year})
+  res.render('lectionary')
 });
 
 app.get('/', function(req, res) {
-  res.render('home', {copyrightYear: year})
+  res.render('home')
 });
 
 app.get('/local-churches', function(req, res) {
-  res.render('local-churches', {copyrightYear: year})
+  res.render('local-churches')
 });
 
 app.get('/mobile-calendar', function(req, res) {
-  res.render('mobile-calendar', {copyrightYear: year})
+  res.render('mobile-calendar')
 });
 
 app.get('/Prayer', function(req, res) {
-  res.render('prayer', {copyrightYear: year})
-});
-
-app.get('/', function(req, res) {
-  res.render('home', {copyrightYear: year})
+  res.render('prayer')
 });
 
 app.get('/privacy', function(req, res) {
-  res.render('privacy', {copyrightYear: year})
+  res.render('privacy')
 });
 
 app.get('/sermons', function(req, res) {
-  res.render('sermons', {copyrightYear: year})
+  res.render('sermons')
 });
 
 app.get('/template', function(req, res) {
-  res.render('template', {copyrightYear: year})
+  res.render('template')
 });
-
-
 
 app.listen(3000, function() {
   console.log('Server started on port 3000');
